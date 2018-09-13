45dd028c5632454650669c97caac7e802f2bc6a1
Jackson <Jmarks119@gmail.com>
2018-09-12 18:54:29 -0700

    Welcome screen, initial packages set up.

---
+++ LedgerInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Drawing.Color;
+using Alba.CsConsoleFormat;
+using Alba.CsConsoleFormat.Fluent;
+
+namespace MarksBankLedger
+{
+    public class LedgerInterface
+    {
+        public static void PrintWelcome()
+        {
+            Colorful.Console.WriteAscii("Marks Bank", DarkGreen);
+            Colors.WriteLine("Welcome to the Marks Bank's console ledger access point. Please log in with your account in order to access ".Blue(),
+                "your balance and withdraw or deposit funds. If you do not yet have an account with Marks Bank then creating one is as easy ".Blue(),
+                "as providing an e-mail. Press any key to continue to the main menu.".Blue());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarksBankLedger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            LedgerInterface.PrintWelcome();
+            Console.ReadKey();
+        }
+    }
+}
